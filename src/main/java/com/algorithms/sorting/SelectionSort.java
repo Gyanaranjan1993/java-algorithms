9bc570099a095f163d69473e40454e9498b097bf
Gyana Ranjan Mohanty <gyanapuspa123@gmail.com>
2024-03-22 00:20:52 -0400

    Initial commit

---
+++ src/main/java/com/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,17 @@
+package com.algorithms.sorting;
+
+public class SelectionSort {
+    public void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
